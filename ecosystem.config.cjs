f7c83e81f1c39ef3b192bcebb6ccbc8a6b9b1454
Meet Bhungaliya <meet@Meets-MacBook-Air.local>
2026-04-25 13:41:32 +0530

    update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,23 +2,22 @@ module.exports = {
   apps: [
     {
       name: 'adonis-web-server',
-      script: 'build/bin/server.js',
+      script: 'npm',
+      args: 'run dev',
       instances: 1,
       exec_mode: 'fork',
       max_memory_restart: '400M',
-      node_args: '--max-old-space-size=384',
       env: {
         NODE_ENV: 'production',
       },
     },
     {
       name: 'adonis-queue-worker',
-      script: 'build/ace.js',
-      args: 'queue:work --concurrency=3',
+      script: 'npm',
+      args: 'run worker:run',
       instances: 1,
       exec_mode: 'fork',
       max_memory_restart: '200M',
-      node_args: '--max-old-space-size=192',
       env: {
         NODE_ENV: 'production',
       },
